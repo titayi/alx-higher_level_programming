00279d03bbb1f7653b2dd6507baa92cdd27e2851
titayi <kitayimike@gmail.com>
2022-12-20 09:52:27 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" No imported Module """
+
+
+class Square:
+    """ Defined the class """
+    def __init__(self, size):
+        """ Instantiating size """
+
+        self.__size = size
