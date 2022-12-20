26e6678ccea07a2293cd4f3dbf6c1b26889e6569
titayi <kitayimike@gmail.com>
2022-12-20 11:47:56 -0800

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Defining class Square """
+
+
+class Square:
+    """ Defining the square """
+    def __init__(self, size=0):
+        """ Args:
+            self - Param 1
+            size - Param 2
+            Checking for typeError and ValueError
+            Initializing size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+            def area(self):
+                """Returns current square area"""
+                return (self.__size ** 2)
