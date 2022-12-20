5b09c5b0b10e2628dc0e474d10b4f3237d5d9d7b
titayi <kitayimike@gmail.com>
2022-12-20 11:28:00 -0800

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,17 +2,19 @@
 """ Defining class Square """
 
 
-def __init__(self, size=0):
-    """ Args:
+class Square:
+    """ Defining the square """
+    def __init__(self, size=0):
+        """ Args:
             self - Param 1
             size - Param 2
 
             Checking for typeError and ValueError
 
             Initializing size """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    else:
-        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
