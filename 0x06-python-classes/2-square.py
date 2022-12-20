a350a029be231a82972732e8d71b127f0d1840a3
titayi <kitayimike@gmail.com>
2022-12-20 11:14:45 -0800

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Defining class Square """
+
+
+def __init__(self, size=0):
+    """ Args:
+            self - Param 1
+            size - Param 2
+
+            Checking for typeError and ValueError
+
+            Initializing size """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = size
