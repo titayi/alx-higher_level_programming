61332ee1fce7c4d8637c71a4f838d8f22bb31d5d
titayi <kitayimike@gmail.com>
2022-12-21 01:11:58 -0800

    Task 10

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Imported a module """
+
+import math
+""" Defining the MagicClass """
+
+
+class MagicClass:
+    """ Defining th MagicClass """
+
+    def __init__(self, radius=0):
+        """ Args:
+                self - Param 1 
+                radius - Param 2 
+
+                Initializing """
+        self.__radius = radius
+
+        if type(radius) is not and type(radius) is not float:
+            raise TypeError("Radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """ Return the area """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """ Return the circumference """
+        return 2 * math.pi * self.__radius
