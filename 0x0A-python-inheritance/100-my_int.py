7f70249f654a0458ea12e3f60271dd568b265b94
titayi <kitayimike@gmail.com>
2023-01-09 12:19:21 -0800

    Advanced Tasks

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Defines a class MyInt that inherits from int
+"""
+
+
+class MyInt(int):
+    """
+        Class MyInt
+    """
+    def __eq__(self, value):
+        """
+            Override == opeartor with != behavior
+        """
+        return self.real != value
+
+    def __ne__(self, value):
+        """
+            Override != operator with == behavior
+        """
+        return self.real == value
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Adds a new attribute to an object if it’s possible
+"""
+
+
+def add_attribute(obj, att, value):
+    """
+        Add a new attribute to an object if possible
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+
+    setattr(obj, att, value)
