8ae71064e6fa969c5d7aae57319891d5b0267ab7
titayi <kitayimike@gmail.com>
2023-01-10 12:14:02 -0800

    Hot fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+"""Module: 7-add_item
+This is a script that adds all arguments to a Python list.
+Save them to a file.
+"""
+
+
+import json
 import sys
 import os.path
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""
-module 9-add_item
-"""
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+new_file = "add_item.json"
 new_list = []
-filename = "add_item.json"
-for item in range(len(sys.argv)):
-    if item > 0:
-        new_list.append(sys.argv[item])
-if os.path.exists(filename):
-    my_list = load_from_json_file(filename)
-    for iter2 in new_list:
-        my_list.append(iter2)
-    save_to_json_file(my_list, filename)
-else:
-    save_to_json_file(new_list, filename)
+
+if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
+    new_list = load_from_json_file(new_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        new_list.append(elem)
+
+save_to_json_file(new_list, new_file)
