851478c9366344be9e76381fd7a7b86c1dd9c02e
titayi <kitayimike@gmail.com>
2023-03-18 15:09:06 -0700

    Initial Commit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module MySQLdb """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            port=3306,
+            db=sys.argv[3]
+        )
+    except MySQLdb.Error:
+        print("Error Connecting...")
+    cur = db.cursor()
+    try:
+        cur.execute("SELECT * FROM states WHERE name = %s\
+                     ORDER BY states.id", (sys.argv[4],))
+        results = cur.fetchall()
+        for result in results:
+            print(result)
+    except MySQLdb.Error:
+        print("Execution failed")
+    cur.close()
+    db.close()
