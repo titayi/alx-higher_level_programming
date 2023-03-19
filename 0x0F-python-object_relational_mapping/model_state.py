e321da953a97cd42463918f5be252e68b6a9fba6
titayi <kitayimike@gmail.com>
2023-03-19 12:08:27 -0700

    Task Solutions

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" sqlalchemy Module """
+
+
+from sqlalchemy import create_engine, Sequence, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class State(Base):
+    """ Class for State object """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
