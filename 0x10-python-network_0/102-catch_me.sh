d3834b136c091c66c648af208587a3effb624916
titayi <kitayimike@gmail.com>
2023-03-30 12:38:25 -0700

    Updates

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "You got me!" -L -o /dev/null -w '%{http_code}\n' &
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "You got me!" -L -o /dev/null -w "%{http_code}\n"
