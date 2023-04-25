c68591281e2b703e66ddbb2c7cccd9c6c69d41ca
titayi <kitayimike@gmail.com>
2023-04-25 19:23:49 +0000

    Task Solution

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that writes a string to a file
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
