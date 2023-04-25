6acf751b830d4c481af11caf512a35755772da93
titayi <kitayimike@gmail.com>
2023-04-25 19:31:56 +0000

    Task Solution

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// script that display the status code of a GET request
+
+const request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
